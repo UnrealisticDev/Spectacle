6dc2a6298595677edba5b8207fae9839f21d79e7
Mustafa <mustafa.moiz125@gmail.com>
2021-03-09 14:50:44 -0800

    Initialized project with main function

---
+++ Garganta/Source/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "git2.h"
+
+int main()
+{
+	// Download the desired files
+	std::cout << "Jibbs";
+
+	// Step through files and compile list of specifier counts/file
+	std::cout << "Comp";
+
+	// Post data to relevant server (Contentful most likely)
+
+}
