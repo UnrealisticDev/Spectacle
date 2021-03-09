211e120bee97ef44fc2977cc15e583e4138e9b73
Mustafa <mustafa.moiz125@gmail.com>
2021-03-09 10:07:08 -0800

    Add support for filepath argument.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <filesystem>
 #include "CoreTypes.h"
 #include "Lexer.h"
 #include "Parser.h"
 
 int main(int ArgumentCount, char* Arguments[])
 {
-	FString Filename = "Test.txt";
-	FString FileContents = "";
+	if ( ArgumentCount < 2 )
+	{
+		std::cerr << "No filepath argument provided." << std::endl;
+		return -1;
+	}
+
+	FString Filename = Arguments[1];
+	if ( !std::filesystem::exists(Filename) )
+	{
+		std::cerr << "File not found: " << Arguments[1] << std::endl;
+		return -1;
+	}
+
+	FString FileContent;
 
 	std::ifstream File(Filename);
-	if (File.is_open())
+	if ( File.is_open() )
 	{
 		FString Line = "";
-		while (!File.eof())
+		while ( !File.eof() )
 		{
 			std::getline(File, Line);
-			FileContents += Line + "\n\r"; // Normalize line endings
+			FileContent += Line + "\n\r"; // Normalize line endings
 		}
 	}
 
 	FLexer Lexer;
 	FParser Parser;
 
-	Parser.IdentifyUnrealSpecifiers(Lexer.Tokenize(FileContents.c_str()));
+	FParser::Dump(Parser.IdentifyUnrealSpecifiers(Lexer.Tokenize(FileContent.c_str())));
 }
