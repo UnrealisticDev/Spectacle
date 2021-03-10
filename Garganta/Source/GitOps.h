b2824277b743149ed28cac52c058e1df40759516
Mustafa <mustafa.moiz125@gmail.com>
2021-03-09 17:25:20 -0800

    Moved git cloning and fixup responsibilities to separate file.

---
+++ Garganta/Source/GitOps.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "git2.h"
+
+struct FGitOps
+{
+	/** Clones the requested repository into a working directory. */
+	static bool CloneDirectory(const char* RepoURL, const char* WorkingDirectory)
+	{
+		git_libgit2_init();
+
+		git_repository* repo = nullptr;
+		if ( git_clone(&repo, RepoURL, WorkingDirectory, nullptr) != 0 )
+		{
+			std::cerr << "Clone failed: " << git_error_last()->message << std::endl;
+			return false;
+		}
+
+		git_libgit2_shutdown();
+
+		return FixupWorkingDirectoryPermissions(WorkingDirectory);
+	}
+
+private:
+
+	/**
+	 * Removes permission restrictions set by
+	 * git clone process from provided directory.
+	 */
+	static bool FixupWorkingDirectoryPermissions(std::string WorkingDirectory)
+	{
+		namespace fs = std::filesystem;
+
+		for (const fs::directory_entry& Path : fs::recursive_directory_iterator(WorkingDirectory))
+		{
+			try
+			{
+				fs::permissions(Path, fs::perms::all);
+			}
+			catch (std::exception&)
+			{
+				std::cout << "Error setting permissions on working directory.";
+				return false;
+			}
+		}
+
+		return true;
+	}
+};
