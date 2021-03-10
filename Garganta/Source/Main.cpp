ea76eb34bc9baab2dbd32b852d44290d15b97664
Mustafa <mustafa.moiz125@gmail.com>
2021-03-09 17:29:35 -0800

    Move working directory cleanup into function

--- Garganta/Source/Main.cpp
+++ Garganta/Source/Main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <filesystem>
-#include "corecrt_io.h"
-#include "direct.h"
 #include "GitOps.h"
+
+bool CleanupWorkingDirectory()
+{
+	try
+	{
+		std::filesystem::remove_all("Temp");
+	}
+	catch (std::filesystem::filesystem_error E)
+	{
+		std::cerr << "Working directory cleanup failed: " << E.what();
+		return false;
+	}
+
+	return true;
+}
 
 int main()
 {
-	// Clone relevant files into working directory
 	const std::string WorkingDirectory = "Temp";
 	if ( !FGitOps::CloneDirectory("https://github.com/Mowgl33/Rebaser.git", WorkingDirectory.c_str()) )
 	{
@@ -18,14 +30,8 @@ int main()
 	
 	// @TODO: Post data to relevant server (Contentful most likely)]
 	
-	// Cleanup working directory
-	try 
+	if ( !CleanupWorkingDirectory() )
 	{
-		std::filesystem::remove_all("Temp");
-	}
-	catch (std::filesystem::filesystem_error e)
-	{
-		std::cerr << "Working directory cleanup failed: " << e.what();
 		return -1;
 	}
 
