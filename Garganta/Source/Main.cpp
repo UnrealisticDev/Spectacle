3f3802bd0b7029d0cde65d3fdd6105a0acdaee58
Mustafa <mustafa.moiz125@gmail.com>
2021-03-09 17:25:41 -0800

    Define structure for file parsing.

--- Garganta/Source/Main.cpp
+++ Garganta/Source/Main.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
-#include "git2.h"
+#include <filesystem>
+#include "corecrt_io.h"
+#include "direct.h"
+#include "GitOps.h"
 
 int main()
 {
-	// Download the desired files
-	std::cout << "Jibbs";
+	// Clone relevant files into working directory
+	const std::string WorkingDirectory = "Temp";
+	if ( !FGitOps::CloneDirectory("https://github.com/Mowgl33/Rebaser.git", WorkingDirectory.c_str()) )
+	{
+		return -1;
+	}
 
 	// Step through files and compile list of specifier counts/file
-	std::cout << "Comp";
 
-	// Post data to relevant server (Contentful most likely)
+	
+	// @TODO: Post data to relevant server (Contentful most likely)]
+	
+	// Cleanup working directory
+	try 
+	{
+		std::filesystem::remove_all("Temp");
+	}
+	catch (std::filesystem::filesystem_error e)
+	{
+		std::cerr << "Working directory cleanup failed: " << e.what();
+		return -1;
+	}
 
+	return 0;
 }
