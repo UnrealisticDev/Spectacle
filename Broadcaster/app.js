8a90cff157206e83f668579e6faf9ffd34f4a1cb
Mustafa <mustafa.moiz125@gmail.com>
2021-03-10 18:06:28 -0800

    Receive and print parsed data from Garganta.

--- Broadcaster/app.js
+++ Broadcaster/app.js
@@ -1,3 +1,15 @@
 'use strict';
+const https = require('https');
+const fs = require('fs');
 
-console.log('Hello world');
+console.log('Hello world!');
+setTimeout(() => { console.log("Goodbye"); }, 5000);
+
+console.log(process.argv[2]);
+var databasePath = process.argv[2];
+fs.readFile(databasePath, 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(data);
+});
