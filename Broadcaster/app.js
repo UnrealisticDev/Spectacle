d64d005c1b89434f2dbd843067fb16e86d69ea3c
Mustafa <mustafa.moiz125@gmail.com>
2021-03-11 18:08:23 -0800

    Load dataset from file.

--- Broadcaster/app.js
+++ Broadcaster/app.js
@@ -1,15 +1,44 @@
 'use strict';
-const https = require('https');
 const fs = require('fs');
 
-console.log('Hello world!');
-setTimeout(() => { console.log("Goodbye"); }, 5000);
+/** 
+ * Ensure that parsed dataset has been passed to us from Garganta. 
+ * Otherwise, what are we even here for?! 
+ */
+const parsedDatasetPath = process.argv[2];
+if ( !parsedDatasetPath ) {
+    return console.error("No parsed dataset file provided.")
+}
+console.log("Received dataset file: " + parsedDatasetPath);
+console.
 
-console.log(process.argv[2]);
-var databasePath = process.argv[2];
-fs.readFile(databasePath, 'utf8', function (err, data) {
-	if (err) {
-		return console.log(err);
-	}
-	console.log(data);
-});
+console.log("Loading dataset.");
+var parsedDataset;
+try { 
+    parsedDataset = JSON.parse(fs.readFileSync(parsedDatasetPath, 'utf8'));
+    if ( !parsedDataset || !parsedDataset.data ) {
+        return console.error("Provided dataset was empty.");
+    }
+} catch (error) {
+    return console.log("Failed to open or parse dataset file: " + error);
+}
+console.log("Loaded dataset successfully.");
+console.log("Dataset contains %d entries", parsedDataset.data.length);
+
+/**
+ * Request all current specifier entries from Contentful
+ * to be compared against and operated on.
+ * (Down the line could potentially pull only entries that match dataset)
+ */
+
+// const client = contentful.createClient({
+//     space: '4z1i7rtd5bsh',
+//     environment: 'master',
+//     accessToken: 'Xeqkswi8vvM5us2jEa7ESxU4MyAqyZQj5EHQ1SyzTe8'
+// });
+
+// client.getEntries({
+//     content_type: 'unrealSpecifier'
+// }).then((response) => console.log(response.items)).catch(console.error);
+
+setTimeout(() => { console.log('Timed out.') }, 20000);
