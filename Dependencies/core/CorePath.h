1db74c477f301429f4fa962cf815b5f17d5fcd71
Mustafa <mustafa.moiz125@gmail.com>
2021-03-14 00:55:50 -0800

    Fixup aliasing issues.
    Changed FPaths to namespace to allow aliasing and added forward declaration to prevent compile error for CreateTempDirectory.

--- Dependencies/core/CorePath.h
+++ Dependencies/core/CorePath.h
@@ -1,35 +1,36 @@
 #include <filesystem>
 
-struct FPaths
+namespace FPaths
 {
-    namespace fs = std::filesystem;
-    using path = fs::path;
+	namespace fs = std::filesystem;
+	using path = fs::path;
 
     /** Creates the temporary directory if it does not exist. */
-    static void CreateTempDirectory()
+    path TempDirectory();
+    void CreateTempDirectory()
     {
-        if (!fs::exists(TempDirectory()))
+        if (!fs::exists(FPaths::TempDirectory()))
         {
             fs::create_directory(TempDirectory());
         }
     }
 
     /** Returns the path to the temporary directory. */
-    static path TempDirectory()
+    path TempDirectory()
     {
         static const path Dir = fs::temp_directory_path().append("Spectacle\\");
         return Dir;
     }
 
     /** Returns the path to the source directory. */
-    static path SourceDirectory()
+    path SourceDirectory()
     {
         static const path Dir = TempDirectory().append("Source\\");
         return Dir;
     }
 
     /** Returns the path to the results directory. */
-    static path ResultsDirectory()
+    path ResultsDirectory()
     {
         static const path Dir = TempDirectory().append("Results\\");
         return Dir;
